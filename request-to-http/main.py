046ce4052df2f46b35992fed656752fe8b6c5482
Mateusz Holz <holz.mateusz@gmail.com>
2018-03-15 11:41:18 +0100

    Deleted option to delete added records.

--- request-to-http/main.py
+++ request-to-http/main.py
@@ -1,6 +1,7 @@
 import http.client
 import json
 import copy
+import msvcrt
 
 def getBearerId():
     localFile = open("bearer.txt", 'r')
@@ -24,18 +25,15 @@ def runQuerry(connection, amountOfQuerries, shouldAddIndex, listIDS):
             connection.request("POST", '/ht/config/queries', body=json.dumps(localBody), headers=header)
             response = connection.getresponse()
             localResponse = response.read()
-            print(json.loads(localResponse))
-            listIDS.append(json.loads(localResponse)['id'])
             createdEntries = createdEntries + 1
-            print("Status code: {}, message: {}, entry ID: {}".format(response.status, response.reason, json.loads(localResponse)['id']))
+            print("Status code: {}, message: {}".format(response.status, response.reason))
         else:
             connection.request("POST", '/ht/config/queries', body=json.dumps(globalBody), headers=header)
             response = conn.getresponse()
             response.read()
             localResponse = response.read()
-            listIDS.append(json.loads(localResponse)['id'])
             createdEntries = createdEntries + 1
-            print("Status code: {}, message: {}, entry ID: {}".format(response.status, response.reason, json.loads(localResponse)['id']))
+            print("Status code: {}, message: {}".format(response.status, response.reason))
     print("Successfully created {} entires.".format(createdEntries))
 def getConfig(idx): # 0=liczbaQuerry, 1=numerowanieID
     localFile = open("config.json", 'r')
@@ -48,22 +46,11 @@ def getConfig(idx): # 0=liczbaQuerry, 1=numerowanieID
     else:
         raise TypeError("ID out of possible range")
 
-def clearCreatedEntries(connection, listOfID):
-    for i in listOfID:
-        connection.request("DELETE", '/ht/adhoc/{}'.format(i))
-        response = connection.getresponse()
-        response.read()
-        print(response.status, response.reason)
-        print("Cleared entry id: {}".format(i))
-
 header = {"Content-type": "application/json", "Authorization": "Bearer {}".format(getBearerId())}
 conn = http.client.HTTPSConnection('ai6u732cj7.execute-api.us-east-1.amazonaws.com')
 listaID = []
 
 runQuerry(conn, getConfig(0), getConfig(1), listaID)
-localChar = input("Clear just created entries? y/n ")
-if(localChar == 'y'):
-    clearCreatedEntries(conn, listaID)
-else:
-    print("No entries have been deleted.")
+print("\nPress any key to continue")
+msvcrt.getch()
 conn.close()
