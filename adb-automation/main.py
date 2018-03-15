f2c9fa60f24811c6c41c012dfcd5f84dcac961cc
Mateusz Holz <holz.mateusz@gmail.com>
2018-03-15 12:21:18 +0100

    Initial version of support for multiple connected devices.

--- adb-automation/main.py
+++ adb-automation/main.py
@@ -21,39 +21,37 @@ def getPathOfBuilds():
     buildsDir = r"{}\builds".format(curDir)
     return buildsDir
 
-def getListOfDevicesID():
-    listOfID = []
-    rawJsonData = open(r"\\192.168.64.200\byd-fileserver\MHO\devices.json", 'r').read()
-    jsonData = json.loads(rawJsonData)
-    for i in range(len(jsonData['Devices'])):
-        listOfID.append(jsonData['Devices'][i]['id'])
-    return listOfID
+def loadJsonData(file):
+    jsonData = json.loads(open(file, 'r').read())
+    return jsonData
 
-def installBuilds(operation, buildsDir, ext, adbpath, listOfDevices=None):
+def installBuilds(operation, buildsDir, ext, adbpath, listOfDevices):
     print("{} builds...".format(operation))
     builds = os.listdir(buildsDir)
-    for i in builds:
-        if ext in i:
-            if listOfDevices == None:
-                print("Trying to install {}...".format(i))
+    for j in listOfDevices:
+        for i in builds:
+            if ext in i:
+                print("Trying to install {} on device {}".format(i, j))
                 try:
-                    subprocess.check_output(r"{}adb install -r {}\{}".format(adbpath, buildsDir, i))
-                    print("Installed package {}".format(i))
+                    subprocess.check_output(r"{}adb -s {} install -r {}\{}".format(adbpath, j, buildsDir, i))
+                    print("Installed package {} on device {}".format(i, j))
                 except subprocess.CalledProcessError:
                     continue
-        else:
-            print("No builds found.")
-            break
+            else:
+                print("No builds found.")
+                break
     print("All operations are done.")
 
-def uninstallExistingBuilds(listOfPkgName, adbpath):
+def uninstallExistingBuilds(listOfPkgName, adbpath, listOfDevices):
     print("Uninstalling builds...")
-    for i in listOfPkgName:
-        try:
-            print("Uninstalling {} ...".format(i))
-            subprocess.check_output(r"{}adb uninstall {}".format(adbpath, i), stderr=subprocess.STDOUT) #"stderr=subprocess.STDOUT" <- silences java exceptions that occur when we try to uninstall non-existent build
-        except subprocess.CalledProcessError:
-            continue
+    for j in listOfDevices:
+        for i in listOfPkgName:
+            try:
+                print("Uninstalling {} from device {}".format(i, j))
+                print(r"{}adb -s {} uninstall {}".format(adbpath, j, i))
+                subprocess.check_output(r"{}adb -s {} uninstall {}".format(adbpath, j, i), stderr=subprocess.STDOUT) #"stderr=subprocess.STDOUT" <- silences java exceptions that occur when we try to uninstall non-existent build
+            except subprocess.CalledProcessError:
+                continue
     print("Uninstalled all existing apps.")
 
 def adbConnectionStatus(adbpath):
@@ -61,10 +59,12 @@ def adbConnectionStatus(adbpath):
     return r
 
 if __name__ == '__main__':
+    pathToJson = r"\\192.168.64.200\byd-fileserver\MHO\devices.json"
     extension = ".apk"
     msg1 = ("Uninstall all builds from device (specified in config/builds.txt) and install new apks - type and enter a")
     msg2 = ("Overwrite existing builds with apks - type and enter b")
-    listOfIds = getListOfDevicesID()
+    devicesJson = loadJsonData(pathToJson)
+    idsList = []
     print("Checking adb path...")
 
     try:
@@ -83,24 +83,27 @@ if __name__ == '__main__':
             sys.exit()
 
     print("Checking devices...")
+    rawList = subprocess.check_output(r"{}adb devices".format(getPathOfAdb())).rsplit()
+    tempList = rawList[4:] #omitting first 4 elements as they are not neccesary.
+    for i in range(len(tempList)):
+        if i%2 == 0: #every second element in this list is device's ID
+            idsList.append(tempList[i].decode())
 
+    print("##########################################################\n\nWhat should we do now?\n")
 
+    Input = input("{}\n{}\n".format(msg1, msg2))
+    correctInput = False
 
-#    print("##########################################################\n\nWhat should we do now?\n")
-#
-#    Input = input("{}\n{}\n".format(msg1, msg2))
-#    correctInput = False
-#
-#    while correctInput == False:
-#        if Input == 'a' or Input == 'A':
-#            correctInput = True
-#            uninstallExistingBuilds(getListOfBuildsToUninstall(), getPathOfAdb())
-#            installBuilds("Installing", getPathOfBuilds(), extension, getPathOfAdb())
-#        elif Input == 'b' or Input == 'B':
-#            correctInput = True
-#            installBuilds("Overwriting", getPathOfBuilds(), extension, getPathOfAdb())
-#        else:
-#            Input = input("Didn't recognize your input. Try again.\n")
-#    print("\nPress any key to close program.")
-#    msvcrt.getch()
-#    sys.exit()
+    while correctInput == False:
+        if Input == 'a' or Input == 'A':
+            correctInput = True
+            uninstallExistingBuilds(getListOfBuildsToUninstall(), getPathOfAdb(), idsList)
+            installBuilds("Installing", getPathOfBuilds(), extension, getPathOfAdb(), idsList)
+        elif Input == 'b' or Input == 'B':
+            correctInput = True
+            installBuilds("Overwriting", getPathOfBuilds(), extension, getPathOfAdb())
+        else:
+            Input = input("Didn't recognize your input. Try again.\n")
+    print("\nPress any key to close program.")
+    msvcrt.getch()
+    sys.exit()
