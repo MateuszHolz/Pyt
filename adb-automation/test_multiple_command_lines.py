33b048192d3b40d7081d4548bcbf1cc78805314e
Mateusz Holz <holz.mateusz@gmail.com>
2018-03-16 15:23:07 +0100

    Initial version of capturing screenshot, and pulling it to PC.

---
+++ adb-automation/test_multiple_command_lines.py
@@ -0,0 +1,10 @@
+import subprocess
+import sys
+
+adb_shell = subprocess.Popen(r"C:\Users\mho\AppData\Local\Android\sdk\platform-tools\adb shell", stdin = subprocess.PIPE)
+print("1")
+adb_shell.communicate(b"screencap /sdcard/testscreen.jpg")
+print("2")
+subprocess.Popen(r"C:\Users\mho\AppData\Local\Android\sdk\platform-tools\adb pull /sdcard/testscreen.jpg c:\users\mho\desktop")
+print("exit program")
+sys.exit()
