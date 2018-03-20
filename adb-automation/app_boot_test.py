d94460ef66e7b23b29e599b53ecb36df7e807383
Mateusz Holz <holz.mateusz@gmail.com>
2018-03-20 14:17:25 +0100

    Initial version of automated app boot tester.

---
+++ adb-automation/app_boot_test.py
@@ -0,0 +1,11 @@
+import subprocess
+import re
+
+
+adb_shell = subprocess.check_output(r"C:\Users\mho\AppData\Local\Android\sdk\platform-tools\adb shell wm size", stdin = subprocess.PIPE)
+print(re.sub(r'x', ' ', adb_shell.decode().rstrip())[15:])
+try:
+    adb_shell = subprocess.check_output(r"C:\Users\mho\AppData\Local\Android\sdk\platform-tools\adb shell monkey -p com.huuuge.stars.slots 1")
+except subprocess.CalledProcessError:
+    pass
+print(adb_shell)
