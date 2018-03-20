9ac2f88b3c56e5d2a473f01ad093343a58ca717e
Mateusz Holz <holz.mateusz@gmail.com>
2018-03-20 14:17:41 +0100

    changed hardcoded strings into variables.

--- adb-automation/screenshot_capturer.py
+++ adb-automation/screenshot_capturer.py
@@ -1,8 +1,14 @@
 import subprocess
 import sys
+import installer
+import threading
 
-adb_shell = subprocess.Popen(r"C:\Users\mho\AppData\Local\Android\sdk\platform-tools\adb shell", stdin = subprocess.PIPE)
+fileDir = r"/sdcard/"
+fileName = r"testowyscreen.jpg"
+destinationDir = r"c:\users\mho\desktop"
+
+adb_shell = subprocess.Popen(r"{}adb shell".format(installer.getPathOfAdb()), stdin = subprocess.PIPE)
 print("1")
-adb_shell.communicate(b"screencap /sdcard/testscreen.jpg")
+output = adb_shell.communicate("screencap {}\{}".format(fileDir, fileName).encode())
 print("2")
-subprocess.Popen(r"C:\Users\mho\AppData\Local\Android\sdk\platform-tools\adb pull /sdcard/testscreen.jpg c:\users\mho\desktop")
+subprocess.Popen(r"{}adb pull {}{} {}".format(installer.getPathOfAdb(), fileDir, fileName, destinationDir))
