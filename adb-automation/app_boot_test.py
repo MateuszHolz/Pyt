5fc6a492f760fc4e9dbf9e36e48700e6d91636da
Mateusz Holz <holz.mateusz@gmail.com>
2018-03-20 14:51:10 +0100

    removed unneccesary arg

--- adb-automation/app_boot_test.py
+++ adb-automation/app_boot_test.py
@@ -2,7 +2,7 @@ import subprocess
 import re
 
 
-adb_shell = subprocess.check_output(r"C:\Users\mho\AppData\Local\Android\sdk\platform-tools\adb shell wm size", stdin = subprocess.PIPE)
+adb_shell = subprocess.check_output(r"C:\Users\mho\AppData\Local\Android\sdk\platform-tools\adb shell wm size")
 print(re.sub(r'x', ' ', adb_shell.decode().rstrip())[15:])
 try:
     adb_shell = subprocess.check_output(r"C:\Users\mho\AppData\Local\Android\sdk\platform-tools\adb shell monkey -p com.huuuge.stars.slots 1")
