997d01de6658bef8dccf787017486b8b4368123b
Mateusz Holz <holz.mateusz@gmail.com>
2018-03-21 10:45:06 +0100

    Added option to index screenshot depending on indx.txt file.

--- adb-automation/screenshot_capturer.py
+++ adb-automation/screenshot_capturer.py
@@ -3,12 +3,23 @@ import sys
 import installer
 import threading
 
+def getCurFileIndx():
+    with open('indx.txt', 'r') as f:
+        return int(f.read())
+
+def setCurFileIndx():
+    with open('indx.txt', 'w') as f:
+        f.write(str(indx+1))
+
+indx = getCurFileIndx()
 fileDir = r"/sdcard/"
-fileName = r"testowyscreen.jpg"
+fileName = r"screen_{}.png".format(indx)
 destinationDir = r"c:\users\mho\desktop"
 
 adb_shell = subprocess.Popen(r"{}adb shell".format(installer.getPathOfAdb()), stdin = subprocess.PIPE)
 print("1")
 output = adb_shell.communicate("screencap {}\{}".format(fileDir, fileName).encode())
+setCurFileIndx()
 print("2")
 subprocess.Popen(r"{}adb pull {}{} {}".format(installer.getPathOfAdb(), fileDir, fileName, destinationDir))
+print("Done")
