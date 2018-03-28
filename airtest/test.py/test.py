55daa453bcc158f2e3f1f53507dfbb2081dd0dff
Mateusz Holz <holz.mateusz@gmail.com>
2018-03-28 18:02:56 +0200

    airtest

--- airtest/test.py/test.py
+++ airtest/test.py/test.py
@@ -2,8 +2,10 @@ import os
 
 from airtest.core.api import *
 
+test_section = ""
+
 def constructTemplate(file):
-    return Template(r"{}\testsc\{}".format(os.getcwd(), file))
+    return Template(r"{}\testsc\{}\{}".format(os.getcwd(), test_section, file))
 
 def _waitAndTouch(file):
     touch(wait(constructTemplate(file), interval = 1))
@@ -18,25 +20,28 @@ def _swipe(startPoint, endPoint, option):
 def _takeScrnShot(filename):
     snapshot(r"{}\output\{}".format(os.getcwd(), filename))
 
-connect_device("ios:///")
-#r"{}\builds\HuuugeStars-0.1.239-master-(c31644a12b3cd5d5d55a83dd1017476243fa2dfb)-ad-hoc.ipa".format(os.getcwd())
-install("com.huuuge.stars.slots")
-start_app("com.huuuge.stars.slots")
-_waitAndTouch("allow_button.png")
-_waitAndTouch("ok_bigger.png")
-_waitAndTouch("age_confirm_day.png")
-_waitAndTouch("age_confirm_3.png")
-_waitAndTouch("age_confirm_month.png")
-_waitAndTouch("age_confirm_mar.png")
-_waitAndTouch("age_confirm_year.png")
-swipePoint1=exists(constructTemplate("age_confirm_year_swipe_start.png"))
-swipePoint2=exists(constructTemplate("age_confirm_year_swipe_stop.png"))
-for i in range(2):
-    _swipe(swipePoint1, swipePoint2, "points")
+if __name__ == "__main__":
+    test_section = ""
+    connect_device("android:///")
+    install(r"{}\builds\HuuugeStars-0.1.248-master-(4d73c26f241bf1efb26eba008adae91be768e129)-release.apk".format(os.getcwd()))
+    start_app("com.huuuge.stars.slots")
+    test_section = "age_confirm"
+    _waitAndTouch("allow-button.png")
+    _waitAndTouch("green-ok.png")
+    _waitAndTouch("day-1.png")
+    _waitAndTouch("day-3.png")
+    _waitAndTouch("month-jan.png")
+    _waitAndTouch("month-mar.png")
+    _waitAndTouch("year-2010.png")
+    swipePoint1=exists(constructTemplate("age-confirm-year-2007.png"))
+    swipePoint2=exists(constructTemplate("age-confirm-year-2013.png"))
+    for i in range(2):
+        _swipe(swipePoint1, swipePoint2, "points")
+        sleep(3.0)
+    touch(swipePoint1)
     sleep(3.0)
-touch(swipePoint1)
-sleep(3.0)
-_waitAndTouch("age_green_continue.png")
-sleep(10.0)
-_takeScrnShot("Lobby.png")
-uninstall("com.huuuge.stars.slots")
+    _waitAndTouch("green-continue.png")
+    sleep(10.0)
+    test_section = "profile"
+    _takeScrnShot("Lobby.png")
+    uninstall("com.huuuge.stars.slots")
