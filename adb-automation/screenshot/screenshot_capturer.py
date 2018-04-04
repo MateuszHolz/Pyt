dcbb8bb881ad62bdd055b8fb2ef541af21f4ff36
Mateusz Holz <holz.mateusz@gmail.com>
2018-04-04 14:51:54 +0200

    ScreenShotCapturer from multiple devices.

--- adb-automation/screenshot/screenshot_capturer.py
+++ adb-automation/screenshot/screenshot_capturer.py
@@ -1,10 +1,35 @@
 import subprocess
 import os
 from datetime import datetime
+import msvcrt
+import threading
 
 fileDir = r"/sdcard"
-fileName = r"screen_{}.png".format(datetime.now().strftime('%Y-%m-%d-%H-%M-%S'))
+fileName = r"_{}.png".format(datetime.now().strftime('%Y-%m-%d-%H-%M-%S'))
 destinationDir = os.getcwd()
 
-subprocess.check_output(r'adb shell screencap "{}/{}"'.format(fileDir, fileName))
-subprocess.check_output(r'adb pull "{}/{}" {}'.format(fileDir, fileName, destinationDir))
+def takeScreenShot(device):
+    subprocess.check_output(r'adb -s {} shell screencap "{}/{}{}"'.format(device, fileDir, device, fileName))
+    subprocess.check_output(r'adb -s {} pull "{}/{}{}" {}\output'.format(device, fileDir, device, fileName, destinationDir))
+
+def getDevices():
+    devices = []
+    rawList = subprocess.check_output(r"adb devices").rsplit()
+    tempList = rawList[4:]
+    for i in range(len(tempList)):
+        if i%2 == 0:
+            devices.append(tempList[i].decode())
+    print("Devices: {}".format(devices))
+    return devices
+
+def createThreads(func, devices):
+    threads = []
+    for i in devices:
+        localThread = threading.Thread(target=func, args=(i,))
+        threads.append(localThread)
+        localThread.start()
+    for j in threads:
+        j.join()
+
+if __name__ == "__main__":
+    createThreads(takeScreenShot, getDevices())
