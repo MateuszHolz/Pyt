e81eb90db86711c5ae0a0e2d121d2f849034f603
Mateusz Holz <holz.mateusz@gmail.com>
2018-07-02 13:02:09 +0200

    fff

--- tkinter-and-sql/device_managment/retest.py
+++ tkinter-and-sql/device_managment/retest.py
@@ -1,16 +1,27 @@
-import re
+	# import re
 
 
-^[0-9]{4}$
-pattern = r'^\d+(\.\d+)*$'
+	# ^[0-9]{4}$
+	# pattern = r'^\d+(\.\d+)*$'
 
-while True:
-	l = input("word: ")
-	if len(l) > 0:
-		if re.match(pattern, l):
-			print("matches")
-		else:
-			print("doesn't match")
+	# while True:
+	# l = input("word: ")
+	# if len(l) > 0:
+	# 	if re.match(pattern, l):
+	# 		print("matches")
+	# 	else:
+	# 		print("doesn't match")
 
 # while True:
 # 	print(len(input("test: ")))
+from tkinter import *
+
+master = Tk()
+
+group = LabelFrame(master, text="Group", padx=5, pady=5)
+group.pack(padx=10, pady=10)
+
+w = Entry(group)
+w.pack()
+
+mainloop()
