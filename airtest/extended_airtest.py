85c355fbfaedbd322fd64e56d46cf8649c5f6154
mholz <holz.mateusz@gmail.com>
2018-08-02 21:24:37 +0200

    Refactor.

--- airtest/extended_airtest.py
+++ airtest/extended_airtest.py
@@ -17,6 +17,7 @@ class airtestAutomation():
         self.testSection = None
         self.device = self.connectToDevice()
         self.index = 0
+        self.telnetClient = None
         self.currentScreen = None
         self.currentAction = None
         self.templatesDict = {}
@@ -25,13 +26,16 @@ class airtestAutomation():
         if runApp: self.runApp()
 
     def getCurrentTimestamp(self, data):
+        self.setLatestInfo('getCurrentTimestamp', None)
         return datetime.timestamp(datetime.now())
     
     class Telnet(airtestAutomation):
         def __init__(self, ip):
+            self.setLatestInfo('Initializing Telnet object.', None)
             self.connection = telnetlib.Telnet(ip, '1337')
             
         def fetchData(self):
+            self.setLatestInfo('fetchData', None)
             receivedLine = "_"
             data = []
             endOfContentStr = b'\n'
@@ -44,32 +48,24 @@ class airtestAutomation():
             return data
         
         def close(self):
+            self.setLatestInfo('Deleting Telnet object.', None)
             self.connection.close()
     
         def sendTelnetCommand(self, cmd = None):
-            self.setCurrAction('sendTelnetCommand')
-            self.setCurrScreen(None)
+            self.setLatestInfo('sendTelnetCommand', None)
             self.connection.write(cmd.encode('ascii')+b'\r\n')
             return self.fetchData()
             
     def createTelnetClient(self):
-        self.setCurrAction('createTelnetClient')
-        self.setCurrScreen(None)
+        self.setLatestInfo('createTelnetClient', None)
         if not self.telnetClient:
             self.telnetClient = self.Telnet(self.getDeviceIpAddr())
             return self.telnetClient.fetchData()
         else:
             raise Exception('Telnet client was already created before.')
 
-    def closeTelnetClient(self):
-        self.setCurrAction('closeTelnetClient')
-        self.setCurrScreen(None)
-        self.telnetClient.close()
-        self.telnetClient = None
-
     def connectToDevice(self):
-        self.setCurrAction('connectToDevice')
-        self.setCurrScreen(None)
+        self.setLatestInfo('connectToDevice', None)
         try:
             device = connect_device('android:///{}'.format(self.devId))
         except Exception:
@@ -78,34 +74,30 @@ class airtestAutomation():
         return device
 
     def clearAppData(self, optionalPckName = None):
+        self.setLatestInfo('clearAppData', None)
         clear_app(optionalPckName) if optionalPckName else clear_app(self.packageName)
 
     def runApp(self):
-        self.setCurrAction('runApp')
-        self.setCurrScreen(None)
+        self.setLatestInfo('runApp', None)
         start_app(self.packageName)
 
     def setTestSection(self, testSection):
-        self.setCurrAction('setTestSection')
-        self.setCurrScreen(None)
+        self.setLatestInfo('setTestSection', None)
         self.testSection = testSection
 
     def getTestSection(self):
-        self.setCurrAction('getTestSection')
-        self.setCurrScreen(None)
+        self.setLatestInfo('getTestSection', None)
         return self.testSection
 
     def waitAndTouch(self, file, sleepTime = 4, timeout = 60, interval = 1, duration = 0.2):
         temp = self.constructTemplate(file)
-        self.setCurrAction('waitAndTouch')
-        self.setCurrScreen(file)
+        self.setLatestInfo('waitAndTouch', file)
         localPos = wait(temp, interval = interval, timeout = timeout)
         touch(localPos, duration = duration)
         sleep(sleepTime)
 
     def constructTemplate(self, file):
-        self.setCurrAction('constructTemplate')
-        self.setCurrScreen(file)
+        self.setLatestInfo(curAc = 'constructTemplate', curSc = file)
         if file in self.templatesDict:
             return self.templatesDict[file]
         else:
@@ -113,21 +105,15 @@ class airtestAutomation():
             self.templatesDict[file] = temp
             return temp
 
-    def setCurrScreen(self, currentScreen):
-        self.currentScreen = currentScreen
+    def setLatestInfo(self, curAc = None, curSc = None):
+        self.currentAction = curAc
+        self.currentScreen = curSc
 
-    def getCurrScreen(self):
-        return self.currentScreen
-
-    def setCurrAction(self, currentAction):
-        self.currentAction = currentAction
-
-    def getCurrAction(self):
-        return self.currentAction
+    def getLatestActions(self):
+        return self.currentAction, self.currentScreen
 
     def swipe(self, startPoint, endPoint, option, duration = 5):
-        self.setCurrAction('swipe')
-        self.setCurrScreen((startPoint, endPoint))
+        self.setLatestInfo(curAc = 'swipe', curSc = (startPoint, endPoint))
         if option == "files":
             temp1 = self.constructTemplate(startPoint)
             temp2 = self.constructTemplate(endPoint)
@@ -149,6 +135,7 @@ class airtestAutomation():
         - down
         Throws exception when direction any other than that has been provided. Accepts only string.
         '''
+        self.setLatestInfo('swipeToDirection', self.getLatestActions()[1])
         low = (0.55, 0.45)
         mid = (0.65, 0.35)
         high = (0.75, 0.25)
@@ -176,20 +163,19 @@ class airtestAutomation():
             raise TypeError('Incorect option chosen for parameter direction: {}. Available options: "left", "right", "up", "down".'.format(direction))
 
     def takeScreenShot(self, filename):
-        self.setCurrAction('takeScreenShot')
-        self.setCurrScreen(None)
+        self.setLatestInfo('takeScreenShot', None)
         fileDir = r"{}\output\{}.png".format(os.getcwd(), filename)
         snapshot(fileDir)
         return fileDir
 
     def getDeviceSize(self):
+        self.setLatestInfo('getDeviceSize', None)
         raw = self.runShellCommand('wm size').split()[2]
         raw = raw.replace('x', ' ').split()
         return float(raw[1]), float(raw[0])
 
     def getErrorImage(self):
-        self.setCurrAction('getErrorImage')
-        self.setCurrScreen(None)
+        self.setLatestInfo('getErrorImage', None)
         errorImg = self.takeScreenShot("error")
         with open(errorImg, 'rb') as f:
             _attachment = MIMEImage(f.read())
@@ -197,8 +183,7 @@ class airtestAutomation():
 
     def getLogcat(self, dir):
         data = self.runShellCommand('logcat -d')
-        self.setCurrAction('getLogcat')
-        self.setCurrScreen(None)
+        self.setLatestInfo('getLogcat', None)
         with open(dir, 'w', encoding='utf-8') as f:
             f.write(data)
         return dir
@@ -219,44 +204,37 @@ class airtestAutomation():
             logcat['Content-Disposition'] = 'attachment; filename="logcat.txt"'
             msg.attach(logcat)
         if subject: msg['Subject'] = subject
+        self.setLatestInfo('sendMail', None)
         server.sendmail(self.auth[0], "mateusz.holz@huuugegames.com", msg.as_string())
         
     def runShellCommand(self, cmd):
-        self.setCurrAction('runShellCommand')
-        self.setCurrScreen(None)
+        self.setLatestInfo('runShellCommand', None)
         return shell(cmd)
 
     def type(self, txt):
-        self.setCurrAction('type')
-        self.setCurrScreen(None)
+        self.setLatestInfo('type', None)
         text(txt)
         sleep(2)
 
     def deleteChar(self, times = 1):
-        self.setCurrAction('deleteText')
-        self.setCurrScreen(None)
+        self.setLatestInfo('deleteChar', None)
         for i in range(times):
             shell('input keyevent 67')
 
     def setIndex(self, index):
-        self.setCurrAction('setIndex')
-        self.setCurrScreen(None)
+        self.setLatestInfo('setIndex', None)
         self.index = index
 
     def getIndex(self, mail = False):
-        if mail == False:
-            self.setCurrAction('getIndex')
-            self.setCurrScreen(None)
+        self.setLatestInfo('getIndex', None)
         return self.index
 
     def wait(self, time):
-        self.setCurrAction('wait')
-        self.setCurrScreen(None)
+        self.setLatestInfo('wait', None)
         sleep(time)
 
     def getDeviceIpAddr(self):
-        self.setCurrAction('getDeviceIpAddr')
-        self.setCurrScreen(None)
+        self.setLatestInfo('getDeviceIpAddr', None)
         output = self.runShellCommand('ip -f inet addr show wlan0')
         ipAddr = output[output.index('inet')+5:output.index('/')]
         print(ipAddr)
@@ -264,15 +242,10 @@ class airtestAutomation():
 
     def returnCoordinatesIfExist(self, file):
         temp = self.constructTemplate(file)
-        self.setCurrAction('returnCoordinatesIfExist')
-        self.setCurrScreen(file)
+        self.setLatestInfo('returnCoordinatesIfExist', file)
         return exists(temp)
 
     def useDeviceBackButton(self, sleepTime = 4):
-        self.setCurrAction('useDeviceBackButton')
-        self.setCurrScreen(None)
+        self.setLatestInfo('useDeviceBackButton', None)
         self.runShellCommand('input keyevent 4')
         sleep(sleepTime)
-    
-    
-            
