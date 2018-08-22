967ffd37bf81140e035f1bd87a908aee03937593
Mateusz Holz <holz.mateusz@gmail.com>
2018-08-22 15:55:51 +0200

    Added some additional testsuites to be performed. Extended info in exception catch.

--- airtest/test_sync.py
+++ airtest/test_sync.py
@@ -2,22 +2,19 @@ import extended_airtest
 import testsuites
 
 def runTests(automat):
-#    try:
-        automat.clearAppData()
-        automat.runApp()
-        automat.type('ad')
-        automat.wait(2)
-        automat.runShellCommand
-        d('logcat -c')
-        testsuites.testTutorial(automat)
+    try:
+        testsuites.testTutorial(automat, True)
+        testsuites.testClubs(automat, True)
+        testsuites.testSocial(automat, True)
+        #testsuites.testLottery(automat, True) ## NOT READY YET
+        testsuites.testNewsfeed(automat, True)
+        testsuites.testLeaderboards(automat, True)
         automat.createTelnet(automat)
-        #testsuites.testSocial(automat)
-        #testsuites.testClubs(automat)
-        testsuites.testLottery(automat)
-        #testsuites.testNewsfeed(automat)
+        automat.telnet.reachLevel(300, skipLobbyPopups = True)
+        testsuites.seekForSlot(automat, 'wheel_of_wins', True)
 
-    #except Exception:
-    #    automat.sendMail(subject = 'Sync Test, Section: {}, Status: Failed'.format(automat.getTestSection()))
+    except Exception as ex:
+        automat.sendMail(subject = 'Sync Test, Section: {}, Status: Failed. Error: {}'.format(automat.getTestSection(), ex))
 
 
 aut = extended_airtest.airtestAutomation('AQH0217A27000562', 'com.huuuge.stars.slots')
